336f3240d5cf9f9d7da558db73fba4d5c86e56b2
dracaryS <56050820+xdracaryS@users.noreply.github.com>
2021-10-28 23:02:22 +0300

    Add files via upload

---
+++ locale-finder.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+localeFileName = "locale_string.txt"
+lines = []
+
+try:
+	lines = open(localeFileName,"r").readlines()
+except:
+	print localeFileName+" <-- can't find this file! "
+	os.system("PAUSE")
+	sys.exit()
+
+cur = 0
+lineIndex = 0
+for line in lines:
+	lineIndex+=1
+	if cur == 2:
+		cur = 0
+		continue
+	firstPos = line.find('"')
+	if firstPos == -1:
+		print "Problem from "+line+" line: "+str(lineIndex)
+		os.system("PAUSE")
+		sys.exit()
+	else:
+		secondPos = line[firstPos+1:].find('";')
+		if secondPos == -1:
+			print "Problem from "+line+" line: "+str(lineIndex)
+			os.system("PAUSE")
+			sys.exit()
+	cur+=1
+
+print "Good job your locale_string is clean! "
+print "by dracaryS "
+os.system("PAUSE")
+sys.exit()
